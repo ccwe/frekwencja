ccedce58d28559c4fb34d7890b42f4f501abc7d3
pinkie-py <59065701+pinkie-py@users.noreply.github.com>
2021-12-17 11:15:38 +0100

    code cleanup
    Removed unneeded code: login form and its handles, api calls

--- frontend/src/scripts/index.ts
+++ frontend/src/scripts/index.ts
@@ -1,13 +1,7 @@
 import '../styles/index.scss';
-import { handleForm, LoginForm, showDemo } from './controls/form';
+import { showDemo } from './controls/form';
 
-const form = document.querySelector<LoginForm>('form')!;
 const demo = document.querySelector('#demo')!;
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  handleForm(form);
-});
 
 demo.addEventListener('click', (e) => {
   e.preventDefault();
