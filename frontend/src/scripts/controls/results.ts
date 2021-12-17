2f5c067a9b04fb35e056ae11f7082d7dd9eb7bc8
pinkie-py <59065701+pinkie-py@users.noreply.github.com>
2021-12-17 15:46:23 +0100

    enable parsing of Hashed Fragment(tm) and fix parsing errors

--- frontend/src/scripts/controls/results.ts
+++ frontend/src/scripts/controls/results.ts
@@ -23,19 +23,36 @@ const AD_SLOT = '__AD_SLOT__';
 
 function parse(data: Results): [ResultsTile, string[]] {
   const results: ResultsTile = {};
+  let ctn = 0;
 
   for (const subject in data.Attendance) {
     const Attendance = data.Attendance[subject];
     const GPA = data.GPAs[subject];
     results[subject] = { Attendance, GPA };
+    ctn += 1;
   }
 
-  results.Overall = { Attendance: data.Attendance.Overall, GPA: data.GPA };
+  results.Overall = {
+    Attendance: Object.values(data.Attendance).reduce((pv, cv) => {
+      return cv.map((v, i) => {
+        return {
+          Value: pv[i].Value + v.Value,
+          Types: v.Types.map((v, j) => v + pv[i].Types[j])
+        };
+      });
+    }),
+    GPA: data.GPA
+  };
+
+  results.Overall.Attendance = results.Overall.Attendance.map((v) => ({
+    ...v,
+    Value: Math.round((v.Value / (ctn || 1)) * 100) / 100
+  }));
 
   return [
     results,
     Object.keys(results).sort((a, b) => {
-      if (a === 'Overall') return 0;
+      if (a === 'Overall') return -1;
       else if (b === 'Overall') return 1;
 
       for (let i = 0; i < 3; i++) {
--- frontend/src/scripts/index.ts
+++ frontend/src/scripts/index.ts
@@ -1,7 +1,18 @@
 import '../styles/index.scss';
-import { showDemo } from './controls/form';
+import sampleData from './controls/demo_data.json';
+import { getStateFromHash } from './controls/hashed_fragment_state';
+import { showResults } from './controls/results';
 
 const demo = document.querySelector('#demo')!;
+const hash = window.location.hash;
+
+if (hash.startsWith('#')) {
+  showResults(getStateFromHash(hash));
+}
+
+function showDemo() {
+  showResults(sampleData);
+}
 
 demo.addEventListener('click', (e) => {
   e.preventDefault();
